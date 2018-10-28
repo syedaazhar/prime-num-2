31941c113bb3bb496f6c35dff93d684662ac7f85
syedaazhar <38943549+syedaazhar@users.noreply.github.com>
2018-10-28 12:49:10 +0500

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,40 @@
+function check() {
+    // let inputVal = parseInt(document.querySelector('input').value);
+    // let i;
+    // for (i = 2; i < 100; i++) {
+    //     if (inputVal % i === 0) {
+    //         break;
+    //     }
+    //     console.log(i);
+    // }
+    // if (inputVal === i) {
+    //     console.log(i);
+    //     alert(i + 'prime numb')
+    // }
+    // else{
+    //     alert('not')
+    // }
+
+}
+
+// function isPrime(numb){
+//     let i;
+//     for (i = 2; i < numb; i++) {
+//         if (numb % i === 0) {
+//             break;
+//         }
+      
+//     }
+//     if (numb === i) {
+//        return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// for(let i=2;i<=100;i++){
+//     if(isPrime(i)){
+//         console.log(i)
+//     }
+// }
